b81e429b26237fc83f0e3d5a73cb3538bde47347
ValLins18 <valdemilson.lins@gmail.com>
2023-05-28 21:33:37 -0300

    Monitoramento de EPIs na tela de perfil

--- Model/AlertModel.cs
+++ Model/AlertModel.cs
@@ -1,8 +1,10 @@
 ﻿namespace EpiConnectFrontEnd.Model {
     public class AlertModel {
         public int AlertId { get; set; }
-        public string DangerousLevel { get; set; }
+        public int? DangerousLevel { get; set; }
         public DateTime AlertDate { get; set; }
+        public bool IsOpen { get; set; } = false;
+        public TimeSpan? UnprotectedTime { get; set; }
         public MetricsModel Metrics { get; set; }
         public EpiModel Epi { get; set; }
     }
